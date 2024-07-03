e62ac04c1b1c54d7a043137145ac95db94ce4e66
Mario Schiavone <s299500@studenti.polito.it>
2024-07-04 00:01:57 +0200

    finito

--- UI/controller.py
+++ UI/controller.py
@@ -9,10 +9,33 @@ class Controller:
         self._model = model
 
     def handle_graph(self, e):
-        pass
-
+        self._model.buildGraph()
+        nNodes, nEdges = self._model.getGraphDetails()
+        self._view.txt_result.controls.clear()
+        self._view.txt_result.controls.append(ft.Text(f"Numero di Nodes: {nNodes}"))
+        self._view.txt_result.controls.append(ft.Text(f"Numero di Edges: {nEdges}"))
+        self._view.txt_result.controls.append(ft.Text(
+            f"Informazioni sui pesi degli archi - valore minimo:  {self._model.getMinWeight()} e valore massimo: {self._model.getMaxWeight()}"
+        ))
+        self._view.update_page()
     def handle_countedges(self, e):
-        pass
+        soglia = float(self._view.txt_name.value)
+        if soglia < self._model.getMinWeight() or soglia > self._model.getMaxWeight():
+            self._view.create_alert("Valore di soglia non valida!")
+            return
+        count_bigger, count_smaller = self._model.countEdges(soglia)
+        self._view.txt_result2.controls.append(ft.Text(f"Numero archi con peso maggiore della soglia: {count_bigger}"))
+        self._view.txt_result2.controls.append(ft.Text(f"Numero archi con peso minore della soglia: {count_smaller}"))
+        self._view.update_page()
 
     def handle_search(self, e):
-        pass
+        threshold = float(self._view.txt_name.value)
+        self._model.searchPath(threshold)
+
+        self._view.txt_result3.controls.append(ft.Text(
+            f"Peso cammino massimo: {str(self._model.computeWeightPath(self._model.solBest))}"))
+
+        for ii in self._model.solBest:
+            self._view.txt_result3.controls.append(ft.Text(
+                f"{ii[0]} --> {ii[1]}: {str(ii[2]['weight'])}"))
+        self._view.update_page()
--- model/model.py
+++ model/model.py
@@ -1,3 +1,98 @@
+import networkx as nx
+
+from database.DAO import DAO
+
+
 class Model:
     def __init__(self):
-        pass
+        self._grafo = nx.DiGraph()
+        self._nodes = []
+        self._edges = []
+
+        self._allChromosomes = DAO.getAllChromosomes()
+        self._allConnectedGenes=DAO.getAllConnectedGenes()
+        self._allGenes=DAO.getAllGenes()
+
+        self.solBest=[]
+
+        self._idMap={}
+        for g in self._allGenes:
+            self._idMap[g.GeneID]=g.Chromosome
+
+    def searchPath(self, t):
+
+        for n in self._grafo.nodes:
+            partial = []
+            partial_edges = []
+
+            partial.append(n)
+            self.ricorsione(partial, partial_edges, t)
+
+        print("final", len(self.solBest), [i[2]["weight"] for i in self.solBest])
+
+    def ricorsione(self, partial, partial_edges, t):
+        n_last = partial[-1]
+        neigh = self.getAdmissibleNeighbs(n_last, partial_edges, t)
+
+        # stop
+        if len(neigh) == 0:
+            weight_path = self.computeWeightPath(partial_edges)
+            weight_path_best = self.computeWeightPath(self.solBest)
+            if weight_path > weight_path_best:
+                self.solBest = partial_edges[:]
+            return
+
+        for n in neigh:
+            partial.append(n)
+            partial_edges.append((n_last, n, self._grafo.get_edge_data(n_last, n)))
+            self.ricorsione(partial, partial_edges, t)
+            partial.pop()
+            partial_edges.pop()
+
+    def getAdmissibleNeighbs(self, n_last, partial_edges, t):
+        all_neigh = self._grafo.edges(n_last, data=True)
+        result = []
+        for e in all_neigh:
+            if e[2]["weight"] > t:
+                e_inv = (e[1], e[0], e[2])
+                if (e_inv not in partial_edges) and (e not in partial_edges):
+                    result.append(e[1])
+        return result
+
+    def computeWeightPath(self, mylist):
+        weight = 0
+        for e in mylist:
+            weight += e[2]['weight']
+        return weight
+
+
+    def buildGraph(self):
+        self._grafo.clear()
+        for chromosome in self._allChromosomes:
+            self._nodes.append(chromosome)
+        self._grafo.add_nodes_from(self._nodes)
+        edges={}
+        for g1, g2, corr in self._allConnectedGenes:
+            if (self._idMap[g1], self._idMap[g2]) not in edges:
+                edges[(self._idMap[g1], self._idMap[g2])]=float(corr)
+            else:
+                edges[(self._idMap[g1], self._idMap[g2])] += float(corr)
+        for key, value in edges.items():
+            self._edges.append((key[0], key[1], value))
+        self._grafo.add_weighted_edges_from(self._edges)
+    def countEdges(self, soglia):
+        count_bigger=0
+        count_smaller=0
+        for edge in self._grafo.edges(data=True):
+            if edge[2]['weight']>soglia:
+                count_bigger += 1
+            elif edge[2]['weight']<soglia:
+                count_smaller += 1
+        return count_bigger, count_smaller
+
+    def getMinWeight(self):
+        return min(x[2]['weight'] for x in self._grafo.edges(data=True))
+    def getMaxWeight(self):
+        return max(x[2]['weight'] for x in self._grafo.edges(data=True))
+    def getGraphDetails(self):
+        return len(self._grafo.nodes), len(self._grafo.edges)
